06a8cca82fcbad099526d5478d543d47301325b4
Simon Rasmussen <zetlon@gmail.com>
2015-04-03 17:49:22 +0200

    global variable fix

--- plugins/matchticker.plugin.rb
+++ plugins/matchticker.plugin.rb
@@ -1,29 +1,26 @@
 require 'nokogiri'
 require 'open-uri'
 
-class Imgur < Plugin
-   
+class Matchticker < Plugin
   def initialize(bot)
     super(bot)
-		
+	
+	@matches = nil
+	
 	@thread = Thread.new {
       while true
 		
-		puts "opening: " + CONFIG::TEAM_LINK
-		
 		doc = Nokogiri::HTML(open(CONFIG::TEAM_LINK))
-				
+		
 		@matches = doc.css('#gb-matches > tbody > tr').map { |link|
 			opp1, opp2 = link.css('.opp')
 			live = link.css('.live-in');
 			
 			text = opp1.text.strip + " vs " + opp2.text.strip + " in " + live.text.strip
 			
-			puts text
-			
 			text
 		}
-		
+				
         sleep(300)		
       end
     }
