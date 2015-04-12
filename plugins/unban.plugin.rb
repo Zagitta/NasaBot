fa186b80ec2db770743da7747f91bd628fa4540d
Simon Rasmussen <zetlon@gmail.com>
2015-04-12 22:30:14 +0200

    badonk unban

--- plugins/unban.plugin.rb
+++ plugins/unban.plugin.rb
@@ -3,11 +3,11 @@ class Unban < Plugin
   def start(user, args)
     @thread = Thread.new {
       while true
-        sleep(300)
-          @bot.send "PRIVMSG #{@bot.channel} :.unban saltymoses"    
+        sleep(60)
+          @bot.send "PRIVMSG #{@bot.channel} :.unban badonkadonk55"    
       end
     }
-    @bot.say("Nice mirc script asplosions.")
+    @bot.say("Starting auto unban.")
   end
 
   def register_functions
